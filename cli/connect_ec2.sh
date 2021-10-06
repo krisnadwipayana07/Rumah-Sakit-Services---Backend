dab2aac42ae7833b32fd1151f25c7167a191d2e6
Boomski99 <krisnadwipayana30@gmail.com>
2021-10-07 01:33:29 +0800

    development(ci/cd): add cli

---
+++ cli/connect_ec2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ssh -i "boomskii_aws.pem" ec2-user@ec2-18-189-194-77.us-east-2.compute.amazonaws.com
---
+++ cli/connect_rds.sh
@@ -0,0 +1,1 @@
+mysql -h rumah-sakit.cwhnid1vyp1m.us-east-2.rds.amazonaws.com -u admin -p
